b2cb2bd2f62bde292d9abf3ddcdd426000bd19e5
Anandmaurya321 <iliveinindia134@gmail.com>
2024-08-20 18:31:26 +0530

    Add files via upload

---
+++ 01_basic/01_operation.js
@@ -0,0 +1,40 @@
+//*****************************operations******************************/
+let con=4;
+let con1=-con;
+// console.log(con1);
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2**3)
+// console.log(3%2);
+// console.log(3/2);
+
+// let str1="hello";
+// let str2=" Anand maurya";
+// let str3=str1+str2;
+// console.log(str3);
+
+// console.log("2"+3)
+// console.log(2+"3");
+// console.log("2"+3+3)
+// console.log(3+3+"2")
+
+
+// console.log(+true)
+// console.log(+"")
+
+// let num1,num2,num3
+// num1 = num2 = num3 = 3+3
+
+// num1++;
+
+// console.log(num1)
+
+// ++num1
+
+// console.log(num1)
+
+
+// console.log(++num1)
+
+// console.log(num1++)
+
